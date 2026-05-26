e4e34e6bcef8611bbd746cbd76fee2763bb9e557
candelariaguzzetti-byte <ariaguzzetti@gmail.com>
2026-05-26 17:56:28 -0300

    Update programa_ventas.py

--- programa_ventas.py
+++ programa_ventas.py
@@ -1,15 +1,15 @@
 # Trabajo Practico N° 2 - Organización Empresarial
 
 inventario = {} # Diccionario a utilizar
+precios = {}
 
 while True: # Bucle para que luego de cada opción vuelvas al menú
 
     print ("Tienda BluestCat")  # Cargamos la bienvenida y el menú de opciones
-    print ("1. Carga Inicial de Productos ")
-    print ("2. Carga de Stock")
-    print ("3. Carga de Precios")
-    print ("4. Visualización de Inventario")
-    print ("54. Consulta de Stock (Existencias Individuales)")
+    print ("1. Carga Inicial de Productos con su Stock")
+    print ("2. Carga de Precios")
+    print ("3. Visualización de Inventario")
+    print ("4. Consulta de Stock (Existencias Individuales)")
     print ("5. Reporte de Agotados")
     print ("6. Alta de Nuevo Producto")
     print ("7. Actualización de Stock (Ingresos)")
@@ -21,8 +21,29 @@ while True: # Bucle para que luego de cada opción vuelvas al menú
 
     match opcion: # Unimos el número dado a la acción a realizar
 
-        case "1":
-            pass
+        case "1": # Carga inicial de productos
+            
+            num_productos = input ("Ingrese el número de productos a cargar ") 
+            if num_productos == " " or num_productos == "": # Si el número está vacío o presionan enter, imprimimos un aviso
+                    print ("No se permiten caracteres vacíos ni letras")
+            else: # Si imprimen un caracter primero nos aseguramos de que sea un digito
+                if num_productos.isdigit(): # Si es un digito realizamos la acción correspondiente a este punto
+                    for a in range (int(num_productos)): # Creamos un ciclo que se repita la cantidad de veces como herramienta fueron pedidas 
+                        productos_cargar = input (f"Ingrese el nombre del producto {a+1} a cargar ") # En cada vuelta pedimos el nombre de la herramienta a cargar 
+                        if productos_cargar == " " or productos_cargar == "" or productos_cargar in inventario: # Validamos que no este vacío ni duplicado
+                            print ("No se permiten caracteres vacíos ni herramientas duplicadas")
+                            break
+                        else: 
+                            stock_inicial = input (f"Ingrese el stock inicial de {productos_cargar}")
+                            if stock_inicial == " " or stock_inicial == "":
+                                print ("No se permiten caracteres vacíos")
+                                break
+                            else: # Si es un nombre válido lo agregamos a la lista de herramientas previamente creada
+                                inventario[productos_cargar.title()] = stock_inicial
+                else:
+                    print ("Por favor ingrese un número") # Si no ingresan un número, imprimimos el aviso
+
+                print (inventario)
         case "2":
             pass
         case "3":
